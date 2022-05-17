7af5e7953324eaf42590e73b0a6c49518a65fb15
rominalodolo <lodoloromina@gmail.com>
2022-05-17 09:53:33 +0200

    update

--- WorkSpace/Chap1/taco-cloud/src/main/java/tacos/web/OrderController.java
+++ WorkSpace/Chap1/taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -2,6 +2,7 @@ package tacos.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -16,5 +17,12 @@ public class OrderController {
 	@GetMapping("/current")
 	public String orderForm() {
 		return "orderForm";
+	}
+
+	@PostMapping
+	public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+		log.info("Order submitted: {}", order);
+		sessionStatus.setComplete();
+		return "redirect:/";
 	}
 }
