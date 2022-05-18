e129ced337a3ed6a6b3efe23b9736b38b057c53d
rominalodolo <lodoloromina@gmail.com>
2022-05-18 13:04:23 +0200

    update

--- WorkSpace/Chap3_sdJPA/taco-cloud/src/main/java/tacos/TacoCloudApplication.java
+++ WorkSpace/Chap3_sdJPA/taco-cloud/src/main/java/tacos/TacoCloudApplication.java
@@ -11,25 +11,27 @@ import tacos.data.IngredientRepository;
 @SpringBootApplication
 public class TacoCloudApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(TacoCloudApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
 
-  @Bean
-  public CommandLineRunner dataLoader(IngredientRepository repo) {
-    return args -> {
-      repo.deleteAll(); // TODO: Quick hack to avoid tests from stepping on each other with constraint violations
-      repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-      repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-      repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-      repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-      repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
-      repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-      repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
-      repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-      repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
-      repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-    };
-  }
+	@Bean
+	public CommandLineRunner dataLoader(IngredientRepository repo) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+				repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+				repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+				repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+				repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+				repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+				repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+				repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+				repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+				repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+			}
+		};
+	}
 
 }
