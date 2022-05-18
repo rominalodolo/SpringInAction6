c21c364d6667aa26d1f1c375dfbf049cbb8edb6b
rominalodolo <lodoloromina@gmail.com>
2022-05-18 14:14:56 +0200

    update

--- WorkSpace/Chap4/taco-cloud/src/main/java/tacos/data/OrderRepository.java
+++ WorkSpace/Chap4/taco-cloud/src/main/java/tacos/data/OrderRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import tacos.TacoOrder;
 
 public interface OrderRepository 
-         extends CrudRepository<TacoOrder, Long> {
+         extends CrudRepository<TacoOrder, String> {
 
 }
