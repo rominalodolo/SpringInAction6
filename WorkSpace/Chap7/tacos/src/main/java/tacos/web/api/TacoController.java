55e01135de78088375fe258cd4e28dc1991c29db
rominalodolo <lodoloromina@gmail.com>
2022-05-20 09:37:05 +0200

    update

--- WorkSpace/Chap7/tacos/src/main/java/tacos/web/api/TacoController.java
+++ WorkSpace/Chap7/tacos/src/main/java/tacos/web/api/TacoController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,47 +18,43 @@ import tacos.Taco;
 import tacos.data.TacoRepository;
 
 @RestController
-@RequestMapping(path = "/api/tacos", // <1>
-		produces = "application/json")
-@CrossOrigin(origins = "http://tacocloud:8080") // <2>
+@RequestMapping(path="/api/tacos",                      // <1>
+                produces="application/json")
+@CrossOrigin(origins="http://tacocloud:8080")        // <2>
 public class TacoController {
-	private TacoRepository tacoRepo;
+  private TacoRepository tacoRepo;
 
-	public TacoController(TacoRepository tacoRepo) {
-		this.tacoRepo = tacoRepo;
-	}
+  public TacoController(TacoRepository tacoRepo) {
+    this.tacoRepo = tacoRepo;
+  }
 
-	@GetMapping(params = "recent")
-	public Iterable<Taco> recentTacos() { // <3>
-		PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
-		return tacoRepo.findAll(page).getContent();
-	}
+  @GetMapping(params="recent")
+  public Iterable<Taco> recentTacos() {                 //<3>
+    PageRequest page = PageRequest.of(
+            0, 12, Sort.by("createdAt").descending());
+    return tacoRepo.findAll(page).getContent();
+  }
 
-	@PostMapping(consumes = "application/json")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Taco postTaco(@RequestBody Taco taco) {
-		return tacoRepo.save(taco);
-	}
+  @PostMapping(consumes="application/json")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Taco postTaco(@RequestBody Taco taco) {
+    return tacoRepo.save(taco);
+  }
 
-//  @GetMapping("/{id}")
-//  public Optional<Taco> tacoById(@PathVariable("id") Long id) {
-//    return tacoRepo.findById(id);
-//  }
+  @GetMapping("/{id}")
+  public Optional<Taco> tacoById(@PathVariable("id") Long id) {
+    return tacoRepo.findById(id);
+  }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
-		Optional<Taco> optTaco = tacoRepo.findById(id);
-		if (optTaco.isPresent()) {
-			return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
-		}
-		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-	}
-
-	/*
-	 * @GetMapping("/{id}") public ResponseEntity<Taco> tacoById(@PathVariable("id")
-	 * Long id) { Optional<Taco> optTaco = tacoRepo.findById(id); if
-	 * (optTaco.isPresent()) { return new ResponseEntity<>(optTaco.get(),
-	 * HttpStatus.OK); } return new ResponseEntity<>(null, HttpStatus.NOT_FOUND); }
-	 */
+  /*
+  @GetMapping("/{id}")
+  public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+    Optional<Taco> optTaco = tacoRepo.findById(id);
+    if (optTaco.isPresent()) {
+      return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+  }
+  */
 
 }
